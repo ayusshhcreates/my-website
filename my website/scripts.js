4c63c96c2d4aa565e2e204e484f88f1ede0e8bba
ayusshhcreates <askohsarma@gmail.com>
2025-01-29 14:43:01 +0530

    Uploading website files

---
+++ my website/scripts.js
@@ -0,0 +1,5 @@
+// You can add more interactive animations here
+document.addEventListener('DOMContentLoaded', () => {
+    const hero = document.querySelector('.hero');
+    hero.classList.add('animate');
+});
